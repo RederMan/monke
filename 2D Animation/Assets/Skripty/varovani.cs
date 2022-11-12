2c8d426eecc264dfeaea966b88df88aedcdb9aac
RederMan <118018949+RederMan@users.noreply.github.com>
2022-11-12 15:10:27 +0100

    Přidání menu (menu hotové na 100%) finalní verze menu

---
+++ 2D Animation/Assets/Skripty/varovani.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class varovani : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(kancelarLoad());
+    }
+
+    IEnumerator kancelarLoad()
+    {
+        yield return new WaitForSeconds(6f);
+        SceneManager.LoadScene("Menu");
+    }
+}
