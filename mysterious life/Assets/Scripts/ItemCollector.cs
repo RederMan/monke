4702e871f5ea28b6540b096e6715ce814cf34b5e
RederMan <118018949+RederMan@users.noreply.github.com>
2022-11-28 18:44:07 +0100

    level

--- mysterious life/Assets/Scripts/ItemCollector.cs
+++ mysterious life/Assets/Scripts/ItemCollector.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
-    private int coin = 0;
+    private int cherries = 0;
 
-    [SerializeField] private Text coinsText;
+    [SerializeField] private Text cherriesText;
 
     [SerializeField] private AudioSource collectionSoundEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Coin"))
+        if (collision.gameObject.CompareTag("Cherry"))
         {
             collectionSoundEffect.Play();
             Destroy(collision.gameObject);
-            coin++;
-            coinsText.text = "Coins: " + coin;
+            cherries++;
+            cherriesText.text = "Cherries: "  + cherries;
         }
     }
 }
